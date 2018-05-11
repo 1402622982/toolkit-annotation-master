c91caaa11e9fa5c57340f9b2ae55c0ef3491749f
wangpei <123456>
2018-05-11 22:39:15 +0800

    test commit

--- src/main/java/com/wangpei/spring/Application.java
+++ src/main/java/com/wangpei/spring/Application.java
@@ -17,6 +17,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
+
+
         SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.addListeners(new ApplicationReadyListener());
         springApplication.run(args);
